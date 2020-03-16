6322ae528248e7b3e11ad7d3798aaaa911b864ad
joffitzer <joffitzer@gmail.com>
2020-03-16 11:52:38 -0400

    reading, appending, and writing to files

--- reading-files.py
+++ reading-files.py
@@ -12,7 +12,6 @@ employee_file = open("employees.txt", "r")
 for employee in employee_file.readlines():
     print(employee)
 
-
 employee_file.close()
 
 # open("employees.txt", "w")
---
+++ writing-to-files.py
@@ -0,0 +1,16 @@
+# employee_file = open("employees.txt", "a")
+# #a for append to add text to the end of the file
+#
+#
+# employee_file.write("\nKelly - Customer Service")
+#
+# employee_file.close()
+
+
+employee_file = open("employees1.txt", "w")
+
+#w will overwrite the whole file
+
+employee_file.write("\nKelly - Customer Service")
+
+employee_file.close()
